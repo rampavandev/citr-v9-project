b715385b6e84f5ad0202a7fafac78ca59d2c3c16
rampavandev <rampavandev@outlook.com>
2025-07-18 00:30:27 +0530

    eslint config wip

---
+++ padre-ginos/eslint.config.mjs
@@ -0,0 +1,3 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,15 +1,29 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
 const APP = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement(
-      "h1",
-      {},
-      "Padre Ginos"
-    )
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Ginos"),
+    React.createElement(Pizza, {
+      name: '"The Pepperoni Pizza',
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
+  ]);
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
+// const root1 = ReactDOM.render(container);
 root.render(React.createElement(APP));
