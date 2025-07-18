6739e55a5c1b03efb3828442eb380af2a65a47f7
rampavandev <rampavandev@outlook.com>
2025-07-19 00:08:09 +0530

    order component

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,18 +1,15 @@
 import { createRoot } from "react-dom/client";
-import  Pizza  from "./Pizzaa";
-
+import Order from "./Order";
 
 const APP = () => {
   return (
     <div>
       <h1>Padre Ginos</h1>
-      <Pizza image={"/public/pizzas/pepperoni.webp"} name="Pepperoni" description="pep, cheesem n stuff" />
-      <Pizza image={"/public/pizzas/hawaiian.webp"} name="Hawaiian" description="ham, pineapple n stuff" />
-      <Pizza image={"/public/pizzas/big_meat.webp"} name="Big Meat" description="french fries and hot dogs" />
+      <Order />
     </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<APP/>);
+root.render(<APP />);
--- padre-ginos/src/Order.jsx
+++ padre-ginos/src/Order.jsx
@@ -1,9 +1,9 @@
 import Pizza from "./Pizzaa";
 
-export default function Order(){
+export default function Order() {
   const pizzaType = "pepperroni";
   const pizzaSize = "M";
-  return(
+  return (
     <div className="order">
       <h2>Create Order</h2>
       <form>
@@ -20,21 +20,48 @@ export default function Order(){
             <label htmlFor="pizza-size">Pizza Size</label>
             <div>
               <span>
-                <input checked={pizzaSize === 'S'} type="radio" name="pizza-size" value="S" id="pizza-s" />
+                <input
+                  checked={pizzaSize === "S"}
+                  type="radio"
+                  name="pizza-size"
+                  value="S"
+                  id="pizza-s"
+                />
                 <label htmlFor="pizza-s">Small</label>
               </span>
               <span>
-                <input checked={pizzaSize === 'M'} type="radio" name="pizza-size" value="M" id="pizza-m" />
+                <input
+                  checked={pizzaSize === "M"}
+                  type="radio"
+                  name="pizza-size"
+                  value="M"
+                  id="pizza-m"
+                />
                 <label htmlFor="pizza-m">Medium</label>
               </span>
               <span>
-                <input checked={pizzaSize === 'L'} type="radio" name="pizza-size" value="L" id="pizza-l" />
+                <input
+                  checked={pizzaSize === "L"}
+                  type="radio"
+                  name="pizza-size"
+                  value="L"
+                  id="pizza-l"
+                />
                 <label htmlFor="pizza-l">Large</label>
               </span>
             </div>
           </div>
+          <button type="submit">Add to Cart</button>
         </div>
+        <div className="order-pizza">
+          <Pizza
+            name="Pepperoni"
+            description="another pepp pizza"
+            image="/public/pizzas/pepperoni.webp"
+          />
+        </div>
+        <p>$13.37</p>
       </form>
     </div>
-  )
+  );
 }
